9d7fe556efc05c1f030526200f51d15d2bfdcc32
Alea Atapasya <aleaatpsya@gmail.com>
2025-05-04 10:41:06 +0700

    Memisahkan composable dari activity dan  Menyiapkan data di ViewModel

---
+++ app/src/main/java/com/aleaatapasya0002/cakecatalogue/model/Daftar.kt
@@ -0,0 +1,7 @@
+package com.aleaatapasya0002.cakecatalogue.model
+
+data class Daftar(
+    val id: Long,
+    val namaKue: String,
+    val deskripsi: String
+)
---
+++ app/src/main/java/com/aleaatapasya0002/cakecatalogue/ui/screen/MainViewModel.kt
@@ -0,0 +1,34 @@
+package com.aleaatapasya0002.cakecatalogue.ui.screen
+
+import com.aleaatapasya0002.cakecatalogue.model.Daftar
+
+class MainViewModel {
+    val data = listOf(
+        Daftar(
+            1,
+            "Choco Donuts",
+            "Donat berisi coklat yang melimpah"
+        ),
+        Daftar(
+            2,
+            "Strawberry Cheese Cookies",
+            "Cookies yang berisi selai strawberry dan diberi taburan keju"
+        ),Daftar(
+            3,
+            "Blueberry Cake",
+            "Kue berisi selai blueberry dan ditaburi oleh potongan buah blueberry"
+        ),Daftar(
+            4,
+            "Pink Lava Cake",
+            "Kue lembut berisi lelehan krim strawberry manis"
+        ),Daftar(
+            5,
+            "Red Velvet Cake",
+            "Kue lembut berwarna merah dengan rasa coklat dan lapisan krim keju yang creamy"
+        ),Daftar(
+            6,
+            "Pink Choco Donuts",
+            "Donat lembut berlapis cokelat pink manis dengan taburan yang menggoda di atasnya"
+        ),
+    )
+}
